effad77bf507447c6c81a8e6d072ff1da73edf94
Abhijeet Jadhav <ethosystem@gmail.com>
2019-10-27 12:16:17 +0530

    Handle Auth

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const mongoose = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+//Passport Config
+
+require('./config/passport')(passport);
+
+//load routes
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.get('/', (req, res) => {
 	res.send('It works');
 });
+
+//use Routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+	mongoURI: 'mongodb://abhijadhavin:abhijeet31@cluster0-9hbmf.mongodb.net/storybooks-dev',
+	googleClientID: '463694277188-cpi25m4qjn6rp612icbcipjd7gcoce9p.apps.googleusercontent.com',
+	googleClientSecret: 'tm2cUa5uSJsAKWLy-nMvVopg'
+}
